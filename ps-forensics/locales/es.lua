63da58ffbac2d2127abc6bcebe087d7f057aab0b
warleynutricionista-jpg <warleynutricionista@gmail.com>
2026-03-24 11:12:06 -0300

    chore(ps-forensics): harden SQL schema with idempotent migrations

---
+++ ps-forensics/locales/es.lua
@@ -0,0 +1,18 @@
+return {
+    ui = {
+        system_name = 'Sistema Forense',
+        access_denied = 'Acceso denegado',
+        access_denied_authorized_only = 'Acceso denegado: solo personal autorizado',
+        officer_fallback_name = 'Oficial',
+    },
+    commands = {
+        open_forensics = 'Abrir Sistema Forense',
+    },
+    common = {
+        error_title = 'Error',
+        usage_title = 'Uso',
+        optional = 'Opcional',
+        player_not_found = 'Jugador no encontrado',
+        too_far_target = 'Demasiado lejos del objetivo',
+    }
+}
