1d4dea92df5c41d849531af17da80dbe1c84db89
warleynutricionista-jpg <warleynutricionista@gmail.com>
2026-03-31 15:38:00 -0300

    feat(ps-mdt): auto cria caso/evidência e procurados ao iniciar relatório

--- ps-mdt/items/items.lua
+++ ps-mdt/items/items.lua
@@ -1,4 +1,16 @@
 return {
+    ['mdttablet'] = {
+        label = 'Tablet Policial',
+        description = 'Tablet oficial para acessar o painel MDT.',
+        weight = 650,
+        stack = false,
+        consume = 0,
+        close = true,
+        client = {
+            image = 'police_tablet.png',
+            export = 'ps-mdt.OpenMDT'
+        },
+    },
     ['mdtcitation'] = {
         label = 'Bloco de Citação MDT',
         description = 'Documento físico usado no fluxo de emissão de citação via MDT.',
@@ -6,6 +18,5 @@ return {
         stack = true,
         consume = 0,
         close = false,
-        client = { export = 'ps-mdt.OpenMDT' },
     },
 }
