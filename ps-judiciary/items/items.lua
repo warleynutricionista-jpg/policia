2a391a296dd46da6dd928851036a0b44c5338f91
warleynutricionista-jpg <warleynutricionista@gmail.com>
2026-03-31 11:03:42 -0300

    feat(ps-judiciary): busca de players, abas de processos e filtro por processo/acusado/tipo

---
+++ ps-judiciary/items/items.lua
@@ -0,0 +1,14 @@
+return {
+    ['judiciary_tablet'] = {
+        label = 'Tablet Jurídico',
+        weight = 700,
+        stack = false,
+        consume = 0,
+        close = true,
+        description = 'Tablet utilizado para consulta, tramitação e gestão jurídica no tribunal.',
+        client = {
+            image = 'judiciary_tablet.png',
+            export = 'ps-judiciary.useJudiciaryTablet'
+        }
+    },
+}
